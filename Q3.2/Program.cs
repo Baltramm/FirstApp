42dc4b5c0f0f025e2fa8cd74c3fc6713b02924ba
Nikita Bochkovskiy <nikita.bochkovsky@yandex.ru>
2022-03-24 21:05:16 +0600

    Добавление @ и #

--- Q3.2/Program.cs
+++ Q3.2/Program.cs
@@ -2,14 +2,17 @@
 
 class MainClass
 {
-	public static void Main(string[] args)
-	{
+    static void Main(string[] args)
+    {
+        const string MyName = "Jane";
 
-		string myName;
-		myName = "Nikita";
+        Console.WriteLine(MyName);
 
-		Console.WriteLine(myName);
-
-		Console.ReadKey();
-	}
+        Console.WriteLine("Привет, мир");
+        Console.WriteLine("Мне 27 лет");
+        Console.WriteLine("My name is Jane");
+        Console.WriteLine('\u0040');
+        Console.WriteLine('\x23');
+        Console.ReadKey();
+    }
 }
